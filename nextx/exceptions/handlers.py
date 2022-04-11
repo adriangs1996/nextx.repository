f182d7910492d0066bc0bda56d8bd8046e5ec560
adriangs1996 <adriangonzalezsanchez1996@gmail.com>
2022-04-10 22:28:36 -0400

    Add exception handlers

---
+++ nextx/exceptions/handlers.py
@@ -0,0 +1,39 @@
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+
+from nextx.domain.viewmodels import DataResponse
+from nextx.exceptions.definitions import (
+    NotFoundError,
+    ForbiddenError,
+    InternalServerError,
+)
+
+
+async def not_found_exception_handler(request: Request, exc: NotFoundError):
+    data = DataResponse(
+        message=f"Resource named: {exc.name} not found!",
+        status_code=status.HTTP_404_NOT_FOUND,
+    )
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=data.dict())
+
+
+async def forbidden_exception_handler(request: Request, exc: ForbiddenError):
+    data = DataResponse(
+        message=f"Access Forbidden!",
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        data=exc.details,
+    )
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=data.dict())
+
+
+async def internal_server_exception_handler(request: Request, exc: InternalServerError):
+    data = DataResponse(
+        message=f"Server failed unexpectedly",
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        data=exc.details,
+    )
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=data.dict())
